4a71651f6222ea28a021878f3fc08c03e4826493
Nriza <108476946+Nriza@users.noreply.github.com>
2022-07-03 16:17:50 +0800

    Delete java/com/example/admin directory

--- java/com/example/admin/Utils.java
+++
@@ -1,8 +0,0 @@
-package com.example.admin;
-
-public class Utils {
-    // HERE PUT EMAIL AND PASSWORD , LIKE example@gmail.com
-
-    public static final  String EMAIL = "clinicdental126@gmail.com";
-    public static final String PASSWORD = "clinicSystem";
-}
